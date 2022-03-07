1007acf5ae4a29c78af462a21768b60cea312b50
murgendrag <murgendra67@gmail.com>
2022-03-07 22:21:49 +0530

    fibonnaci series added

---
+++ fibonnaciseries.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logicalprogramming
+{
+    public class fibonnaciseries
+    {
+        public static void Displayfibonnaciseries()
+        {
+            int n1 = 0, n2 = 1, n3;
+            int number,i;
+         
+            Console.WriteLine("Enter the number of terms in Fibonnci series");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write(n1 +" "  +n2);
+            
+            for (i=2;i<number;i++)
+            {
+                n3 = n1 + n2;
+                Console.Write( n3 + " ");
+                n1 = n2;
+                n2 = n3;
+
+            }
+        }
+    }
+}
