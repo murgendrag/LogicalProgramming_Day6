dfafae5a19f4a042ac3fc6ecaed145199b7e4726
murgendrag <murgendra67@gmail.com>
2022-04-11 15:56:05 +0530

    CouponCode

---
+++ Coupon.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logicalprogramming
+{
+    public class Coupon
+    {
+        public static void DistinctCoupon()
+        {
+            int[] Couponarray = new int[100];
+            Random random = new Random();
+            for (int i = 1; i < 100; i++)
+            {
+                int Number = random.Next(100);
+                Couponarray[i] = Number;
+                Console.WriteLine(Couponarray[i]);
+            }
+         }
+    }
+}
